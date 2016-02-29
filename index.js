80d6d3b6da17bd91e0132d96d22e509f53ecfef6
Jared Allard <jaredallard@outlook.com>
2016-02-29 11:21:32 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * (c) 2016
+ *
+ * cid - a simple Github ci utilizing docker
+ **/
+
+const githubhook = require('githubhook'),
+      docker     = require('dockerode');
+
+// instancing
+const github = githubhook({/* options */});
+
+github.listen();
+
+github.on('*', function (event, repo, ref, data) {
+  console.log('event:', event);
+  console.log('\nrepo:', repo);
+});
